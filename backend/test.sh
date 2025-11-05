8a280649da5f0c1e25c7521eba80a7e65996cd55
Max Petersen <max.petersen@utah.edu>
2025-11-04 23:48:09 -0700

    modified the test script to output code coverage in human-viewable HTML format

--- backend/test.sh
+++ backend/test.sh
@@ -2,4 +2,5 @@
 
 mkdir -p coverage
 go test -coverprofile="coverage/coverage.out" -covermode count -tags test ./...
+go tool cover -html=coverage/coverage.out -o coverage/coverage.html
 gocover-cobertura < coverage/coverage.out > coverage/cobertura-coverage.xml
