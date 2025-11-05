221c4661dc528aa35142fe026437c95d3d249045
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-04 20:12:05 -0700

    Made results more robust and Rust-like

--- frontend/__tests__/common/result.test.ts
+++ frontend/__tests__/common/result.test.ts
@@ -3,20 +3,31 @@
  */
 
 import "@testing-library/jest-dom";
-import { Ok, Err, unwrap, unwrap_err, Result } from "@/common/result";
+import {
+  Ok,
+  Err,
+  unwrap,
+  unwrap_err,
+  Result,
+  unwrap_or,
+  unwrap_or_else,
+  flatten,
+  transpose,
+} from "@/common/result";
+import { Some, None } from "@/common/option";
 
 describe("Result", () => {
-  it("Throws on bad unwrap", () => {
+  it("throws on bad unwrap", () => {
     const result = Err(19);
     expect(() => unwrap(result)).toThrow("unwrap");
   });
 
-  it("Works on good unwrap", () => {
+  it("works on good unwrap", () => {
     const result = Ok(19);
     expect(unwrap(result)).toBe(19);
   });
 
-  it("Throws on bad unwrap_err", () => {
+  it("throws on bad unwrap_err", () => {
     const result = Ok(19);
     expect(() => unwrap_err(result)).toThrow("unwrap");
   });
@@ -26,7 +37,7 @@ describe("Result", () => {
     expect(unwrap_err(result)).toBe(19);
   });
 
-  it("Typeguards work for Oks", () => {
+  it("has typeguards work for Oks", () => {
     function sample(): Result<number, number> {
       return Ok(19);
     }
@@ -48,7 +59,7 @@ describe("Result", () => {
     }
   });
 
-  it("Typeguards work for Errs", () => {
+  it("has typeguards work for Errs", () => {
     function sample(): Result<number, number> {
       return Err(19);
     }
@@ -59,5 +70,60 @@ describe("Result", () => {
     } else {
       expect(result.error).toBe(19);
     }
+  });
+
+  it("works on unwrap_or with err", () => {
+    const result = Err(19);
+
+    expect(result.ok).toBeFalsy();
+
+    const unwrapped = unwrap_or(result, 2);
+
+    expect(unwrapped).toBe(2);
+  });
+
+  it("works on unwrap_or with ok", () => {
+    const result = Ok(19);
+
+    expect(result.ok).toBeTruthy();
+
+    const unwrapped = unwrap_or(result, 2);
+
+    expect(unwrapped).toBe(19);
+  });
+
+  it("works when else throws on unwrap_or_else", () => {
+    const result = Err(19);
+
+    expect(() =>
+      unwrap_or_else(result, () => {
+        throw new Error("Error!");
+      }),
+    ).toThrow();
+  });
+
+  it("works when using flatten", () => {
+    const result = Ok(Err(19));
+    expect(flatten(result)).toStrictEqual(Err(19));
+  });
+
+  it("works when using flatten with an err", () => {
+    const result = Err(19);
+    expect(flatten(result)).toStrictEqual(Err(19));
+  });
+
+  it("works when using transpose", () => {
+    const result = Ok(Some(19));
+    expect(transpose(result)).toStrictEqual(Some(Ok(19)));
+  });
+
+  it("works when using transpose with none", () => {
+    const result = Ok(None());
+    expect(transpose(result)).toStrictEqual(None());
+  });
+
+  it("works when using transpose with an err", () => {
+    const result = Err(19);
+    expect(transpose(result)).toStrictEqual(Some(Err(19)));
   });
 });
--- frontend/src/common/result.ts
+++ frontend/src/common/result.ts
@@ -1,19 +1,63 @@
+import { Option, Some, None } from "./option";
 type Ok<T> = { ok: true; value: T };
 type Err<E> = { ok: false; error: E };
 export type Result<T, E> = Ok<T> | Err<E>;
 export const Ok = <T>(value: T): Ok<T> => ({ ok: true, value });
 export const Err = <E>(error: E): Err<E> => ({ ok: false, error });
 
-export function unwrap<T, E>(result: Result<T, E>): T {
+export function expect<T, E>(result: Result<T, E>, error_message: string): T {
   if (result.ok) {
     return result.value;
   }
-  throw new Error("Bad unwrap of result!");
+  throw new Error(error_message);
 }
 
-export function unwrap_err<T, E>(result: Result<T, E>): E {
+export function unwrap<T, E>(result: Result<T, E>): T {
+  return expect(result, "Bad unwrap of result!");
+}
+
+export function unwrap_or<T, E>(result: Result<T, E>, alt: T): T {
+  return unwrap_or_else(result, () => alt);
+}
+
+export function unwrap_or_else<T, E>(result: Result<T, E>, alt: () => T): T {
+  if (result.ok) {
+    return result.value;
+  }
+  return alt();
+}
+
+export function expect_err<T, E>(
+  result: Result<T, E>,
+  error_message: string,
+): E {
   if (!result.ok) {
     return result.error;
   }
-  throw new Error("Bad unwrap_err of result!");
+  throw new Error(error_message);
+}
+
+export function unwrap_err<T, E>(result: Result<T, E>): E {
+  return expect_err(result, "Bad unwrap_err of result!");
+}
+
+export function flatten<T, E>(result: Result<Result<T, E>, E>): Result<T, E> {
+  if (result.ok) {
+    return result.value;
+  }
+  return result;
+}
+
+export function transpose<T, E>(
+  result: Result<Option<T>, E>,
+): Option<Result<T, E>> {
+  if (result.ok) {
+    if (result.value.some) {
+      return Some(Ok(result.value.value));
+    } else {
+      return None();
+    }
+  } else {
+    return Some(Err(result.error));
+  }
 }
