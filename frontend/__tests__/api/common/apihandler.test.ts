1d1ec1500eed649427dd5bf5c8150849c4ae283f
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-08 16:26:51 -0700

    Fix some teething issues with the API Handler

--- frontend/__tests__/api/common/apihandler.test.ts
+++ frontend/__tests__/api/common/apihandler.test.ts
@@ -4,27 +4,32 @@
 
 import "@testing-library/jest-dom";
 import { ApiHandler, Method } from "@/api/common/apihandler";
+import { Err } from "@/common/result";
 
 describe("API Handler", () => {
   it("can receive a response", async () => {
-    const handler = ApiHandler("http://www.google.com")(Method.GET);
-    const response = await handler("search?q=university of utah");
+    const handler = ApiHandler(
+      "http://api.github.com/users/SathyaTadinada/repos",
+    )(Method.GET);
+    const response = await handler("");
 
+    expect(response.ok).toBeTruthy();
     if (response.ok) {
       expect(response.value).not.toBeNull();
-    } else {
-      fail();
     }
+  });
+
+  it("properly handles a 404", async () => {
+    const handler = ApiHandler("http://google.com/free-ice-cream")(Method.GET);
+    const response = await handler("");
+
+    expect(response).toStrictEqual(Err(404));
   });
 
   it("properly handles a 405 response", async () => {
     const handler = ApiHandler("http://www.google.com")(Method.POST);
     const response = await handler("{status: 'good'}");
 
-    if (response.ok) {
-      fail();
-    } else {
-      expect(response.error).toBe(405);
-    }
+    expect(response).toStrictEqual(Err(405));
   });
 });
