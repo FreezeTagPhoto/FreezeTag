cd17e92886b3bc8fe37e7607fb1bd96f057d4e66
Max Petersen <max.petersen@utah.edu>
2025-11-08 23:12:47 -0700

    fixed backend pipeline bugs

--- backend/test.sh
+++ backend/test.sh
@@ -4,6 +4,8 @@ TAGS='test,sqlite_math_functions,sqlite_foreign_keys'
 
 mkdir -p coverage
 go test -coverprofile="coverage/coverage.out" -covermode count -tags=$TAGS ./...
+TESTPASSED=$?
 go tool cover -html=coverage/coverage.out -o coverage/coverage.html
 GOFLAGS="-tags=$TAGS" gocover-cobertura < coverage/coverage.out > coverage/cobertura-coverage.xml
 go tool cover -func coverage/coverage.out | tail -n 1
+exit $TESTPASSED
