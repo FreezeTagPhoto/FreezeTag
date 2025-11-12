784655bdde8dac4c68c9140e913d1a5b85d68b2f
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-12 14:48:11 -0700

    Add styling for file upload button

--- frontend/src/components/UI/FileUploadButton/FileUploadButton.tsx
+++ frontend/src/components/UI/FileUploadButton/FileUploadButton.tsx
@@ -1,15 +1,19 @@
 import ImageUploader from "@/api/upload/imageuploader";
 import FileChangeHandler from "@/api/upload/filechangehandler";
+import styles from "./FileUploadButton.module.css";
 
 export default function FileUploadButton() {
   return (
     <form action={handleSubmit}>
+      <label htmlFor="file-upload" className={styles.label}> File Upload! </label>
       <input
         type="file"
         onChange={FileChangeHandler}
         multiple
         required
         name="file"
+        className={styles.button}
+        id="file-upload"
       />
     </form>
   );
