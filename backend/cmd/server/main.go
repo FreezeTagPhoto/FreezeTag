fe299bc253207fc919d8b4e91b6483dcfff3b702
ethan <u0911543@utah.edu>
2025-11-13 15:16:50 -0700

    adding support for repositories

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,47 @@
+//go:build !test
+
+package main
+
+import (
+	"freezetag/backend/api/upload"
+	"freezetag/backend/pkg/database"
+	"freezetag/backend/pkg/images"
+	"freezetag/backend/pkg/images/formats"
+	"freezetag/backend/pkg/repositories"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	repo := initRepository()
+	RegisterEndpoints(router, repo)
+	router.Run("localhost:3824") //nolint:errcheck // no need to check return value
+}
+
+func initParserCollection() images.Parser {
+	parserCollection := images.InitParserCollection()
+	if err := parserCollection.RegisterParserFunc("*.{cr3,CR3,nef,NEF,dng,DNG}", formats.ParseRaw); err != nil {
+		log.Fatalf("[ERROR] failed to register RAW parser: %v", err)
+	}
+	if err := parserCollection.RegisterParserFunc("*.{png,jpg,jpeg}", formats.ParseBasic); err != nil {
+		log.Fatalf("[ERROR] failed to register basic parser: %v", err)
+	}
+	return parserCollection
+}
+
+// initRepository initializes the image repository with a SQLite database and an image parser collection.
+func initRepository() repositories.ImageRepository {
+	db, err := database.InitSQLiteImageDatabase("database.db")
+	if err != nil {
+		log.Fatalf("failed to initialize database: %v", err.Error())
+	}
+	parserCollection := initParserCollection()
+	return repositories.InitImageRepository(db, parserCollection)
+}
+
+func RegisterEndpoints(router *gin.Engine, repo repositories.ImageRepository) {
+	upload.InitUploadEndpoint(repo).RegisterEndpoints(router)
+	// Other endpoints would be registered here
+}
--- backend/pkg/repositories/image_repository.go
+++ backend/pkg/repositories/image_repository.go
@@ -1,8 +1,13 @@
 package repositories
 
+import (
+	"freezetag/backend/pkg/database"
+	"freezetag/backend/pkg/images"
+)
+
 type ImageHandleSuccess struct {
-	Id       uint   `json:"id"`
-	Filename string `json:"filename"`
+	Id       database.ImageId `json:"id"`
+	Filename string           `json:"filename"`
 }
 
 type ImageHandleFail struct {
@@ -17,5 +22,51 @@ type Result struct {
 
 type ImageRepository interface {
 	StoreImageBytes(data []byte, filename string) Result
-	RetrieveThumbnail(id uint, quality uint) ([]byte, error)
+	RetrieveThumbnail(id database.ImageId, quality uint) ([]byte, error)
+}
+
+type DefaultImageRepository struct {
+	db database.ImageDatabase
+	parserCollection images.Parser
+}
+
+func InitImageRepository(db database.ImageDatabase, parserCollection images.Parser) *DefaultImageRepository {
+	return &DefaultImageRepository{
+		db: db,
+		parserCollection: parserCollection,
+	}
+}
+
+func errorResult(filename string, err error) Result {
+	return Result{
+		Success: nil,
+		Err: &ImageHandleFail{
+			Reason:   err,
+			Filename: filename,
+		},
+	}
+}
+
+func (repo *DefaultImageRepository) StoreImageBytes(data []byte, filename string) Result {
+	imagedata, err := repo.parserCollection.ParseImage(filename, data)
+	if err != nil {
+		return errorResult(filename, err)
+	}
+
+	id, err := repo.db.AddImage(filename, imagedata)
+	if err != nil {
+		return errorResult(filename, err)
+	}
+
+	return Result{
+		Success: &ImageHandleSuccess{
+			Id:       id,
+			Filename: filename,
+		},
+		Err: nil,
+	}
+}
+
+func (repo *DefaultImageRepository) RetrieveThumbnail(id database.ImageId, quality uint) ([]byte, error) {
+	return repo.db.GetImageThumbnail(id, quality)
 }
