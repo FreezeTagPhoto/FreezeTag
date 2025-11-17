a9f50569edec1730e4525e4e1daebf78df925256
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-17 13:40:45 -0700

    Make image_ids mandatory and remove placeholder code

--- frontend/src/components/Gallery/Gallery.tsx
+++ frontend/src/components/Gallery/Gallery.tsx
@@ -1,56 +1,15 @@
 import styles from "./Gallery.module.css";
 import GalleryImage from "./GalleryImage/GalleryImage";
 
-type Item = {
-  id: string;
-  colSpan: number;
-  rowSpan: number;
-};
-
-const items: Item[] = [
-  { id: "1", colSpan: 3, rowSpan: 2 },
-  { id: "2", colSpan: 2, rowSpan: 2 },
-  { id: "3", colSpan: 3, rowSpan: 2 },
-  { id: "4", colSpan: 3, rowSpan: 2 },
-  { id: "5", colSpan: 5, rowSpan: 3 },
-  { id: "6", colSpan: 3, rowSpan: 2 },
-  { id: "7", colSpan: 3, rowSpan: 2 },
-  { id: "8", colSpan: 6, rowSpan: 2 },
-  { id: "9", colSpan: 6, rowSpan: 2 },
-  { id: "10", colSpan: 6, rowSpan: 2 },
-];
-
 export type GalleryProps = {
   image_ids: number[];
 };
 
 export default function Gallery(props: GalleryProps) {
-  if (props.image_ids) {
-    return (
-      <div className={styles.grid}>
-        {props.image_ids.map((id) => (
-          <GalleryImage key={id} id={id} />
-        ))}
-      </div>
-    );
-  }
   return (
     <div className={styles.grid}>
-      {items.map((it) => (
-        <div
-          key={it.id}
-          className={styles.tile}
-          style={{
-            gridColumn: `span ${it.colSpan}`,
-            gridRow: `span ${it.rowSpan}`,
-          }}
-        >
-          <div className={styles.thumb}>
-            <div className={styles.shapeTri} />
-            <div className={styles.shapeGear} />
-            <div className={styles.shapeSquare} />
-          </div>
-        </div>
+      {props.image_ids.map((id) => (
+        <GalleryImage key={id} id={id} />
       ))}
     </div>
   );
