0de9f98ae01709a2ef141ceacd6e47b1c674051e
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-20 21:21:32 -0700

    Hook up the search bar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+import SearchHandler from "@/api/query/searchhandler";
 import styles from "./page.module.css";
 import Gallery from "@/components/Gallery/Gallery";
 import TopBar from "@/components/TopBar/TopBar";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const images_ids: string | number[] = [];
+  const [images_ids, set_image_ids] = useState<number[]>([]);
+
+  const onChangeHandler = async (query: string) => {
+    const result = await SearchHandler(query);
+    if (result.ok) {
+      set_image_ids(result.value);
+    } else {
+      console.log(
+        "Got " +
+          result.error.status +
+          " from backend with message " +
+          result.error.message,
+      );
+    }
+  };
+
+  useEffect(() => {
+    onChangeHandler("");
+  }, []);
+
   return (
     <>
-      <TopBar />
+      <TopBar onChangeHandler={onChangeHandler} />
       <main className={styles.main}>
         <header className={styles.headerRow}>
           <div>
