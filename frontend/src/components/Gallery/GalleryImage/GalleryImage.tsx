03079a6487773b405a08dcdf0a1a3da02bd62120
Sathya Tadinada <sathya.tadinada@utah.edu>
2025-11-23 23:53:14 -0700

    Updated GalleryImage component to be a clickable button with an action instead of a div

--- frontend/src/components/Gallery/GalleryImage/GalleryImage.tsx
+++ frontend/src/components/Gallery/GalleryImage/GalleryImage.tsx
@@ -2,18 +2,23 @@ import styles from "./GalleryImage.module.css";
 
 export type GalleryImageProps = {
   id: number;
+  onClick?: () => void;
 };
 
-export default function GalleryImage(props: GalleryImageProps) {
+export default function GalleryImage({ id, onClick }: GalleryImageProps) {
   return (
-    <div className={styles.image_container}>
+    <button
+      type="button"
+      className={styles.image_container}
+      onClick={onClick}
+    >
       <img
-        src={`http://localhost:3824/thumbnails/${props.id}?size=1`}
+        src={`http://localhost:3824/thumbnails/${id}?size=1`}
         loading="lazy"
-        alt={`A thumbnail of image ${props.id}`}
+        alt={`A thumbnail of image ${id}`}
         decoding="async"
         className={styles.image}
       />
-    </div>
+    </button>
   );
 }
