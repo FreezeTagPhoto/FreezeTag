e43e9828a80c76731dcdb40a3c0fa904f3b91250
Sathya Tadinada <sathya.tadinada@utah.edu>
2025-11-24 22:45:35 -0700

    Enabled onFocus and buttonRef button elements so that we can enable keyboard nav

--- frontend/src/components/Gallery/GalleryImage/GalleryImage.tsx
+++ frontend/src/components/Gallery/GalleryImage/GalleryImage.tsx
@@ -3,11 +3,24 @@ import styles from "./GalleryImage.module.css";
 export type GalleryImageProps = {
   id: number;
   onClick?: () => void;
+  buttonRef?: (el: HTMLButtonElement | null) => void;
+  onFocus?: () => void;
 };
 
-export default function GalleryImage({ id, onClick }: GalleryImageProps) {
+export default function GalleryImage({
+  id,
+  onClick,
+  buttonRef,
+  onFocus,
+}: GalleryImageProps) {
   return (
-    <button type="button" className={styles.image_container} onClick={onClick}>
+    <button
+      type="button"
+      className={styles.image_container}
+      onClick={onClick}
+      ref={buttonRef}
+      onFocus={onFocus}
+    >
       <img
         src={`http://localhost:3824/thumbnails/${id}?size=1`}
         loading="lazy"
