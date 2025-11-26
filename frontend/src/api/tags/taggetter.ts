8ce637fec5064b5951d549116eb521c6d0a019c4
bjonsson <brayden.f.jonsson@utah.edu>
2025-11-26 11:20:23 -0700

    Finish tests and fix formatting

--- frontend/src/api/tags/taggetter.ts
+++ frontend/src/api/tags/taggetter.ts
@@ -4,8 +4,8 @@ import { ApiHandler, Method, RequestError } from "@/api/common/apihandler";
 import { Result, Err } from "@/common/result";
 
 export type TagGetResult = Result<
-  string[],
-  { status: number; message: string }
+    string[],
+    { status: number; message: string }
 >;
 type TagGetResponse = string[];
 
@@ -15,37 +15,38 @@ type TagGetResponse = string[];
  * @returns A TagGetResult promise
  */
 export default async function TagGetter(
-  image_id?: number,
+    image_id?: number,
 ): Promise<TagGetResult> {
-  return get_tag_with_handler(
-    ApiHandler<TagGetResponse>(SERVER_ADDRESS + "tag/list")(Method.GET),
-    image_id,
-  );
+    return get_tag_with_handler(
+        ApiHandler<TagGetResponse>(SERVER_ADDRESS + "tag/list")(Method.GET),
+        image_id,
+    );
 }
 
 async function get_tag_with_handler(
-  handler: (data: BodyInit) => Promise<Result<TagGetResponse, RequestError>>,
-  image_id?: number,
+    handler: (data: BodyInit) => Promise<Result<TagGetResponse, RequestError>>,
+    image_id?: number,
 ): Promise<TagGetResult> {
-  let result = await (image_id ? handler(`/${image_id}`) : handler(""));
+    let result = await (image_id ? handler(`/${image_id}`) : handler(""));
 
-  if (!result.ok) {
-    const status = result.error.status_code;
-    if (status == 400) {
-      return Err({
-        status,
-        message: ((await result.error.response.json()) as { error: string })
-          .error,
-      });
-    } else {
-      return Err({
-        status,
-        message: await result.error.response.text(),
-      });
+    if (!result.ok) {
+        const status = result.error.status_code;
+        if (status == 400) {
+            return Err({
+                status,
+                message: (
+                    (await result.error.response.json()) as { error: string }
+                ).error,
+            });
+        } else {
+            return Err({
+                status,
+                message: await result.error.response.text(),
+            });
+        }
     }
-  }
 
-  return result;
+    return result;
 }
 
 export const testing_TagGetter = get_tag_with_handler;
