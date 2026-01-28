a6318df4c0519ca6b2217fd7e774b2bede95ecae
Ethan Collier <u0911543@utah.edu>
2026-01-27 23:25:39 -0700

    added tests for some API endpoints

--- backend/api/login/login_test.go
+++ backend/api/login/login_test.go
@@ -1,84 +1,78 @@
-package createuser
+package login
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+
+	// "fmt"
 	"freezetag/backend/api"
 	mockUserService "freezetag/backend/mocks/AuthService"
-	"freezetag/backend/pkg/database"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T) {
+func TestLogin(t *testing.T) {
 	plaintextPassword := "securepassword"
-	expectedUser := &database.PublicUser{
-		ID:        1,
-		Username:  "newuser",
-		CreatedAt: time.Now().Unix(),
-	}
 
 	NewMockAuthService := mockUserService.NewMockAuthService(t)
 	NewMockAuthService.EXPECT().
-		AddUser("newuser", plaintextPassword).
-		Return(expectedUser, nil).Once()
+		AuthenticateUser("testuser", plaintextPassword).
+		Return("json_token", nil).Once()
 
 	router := gin.Default()
-	loginCredentials := api.LoginCredentials{
-		Username: "newuser",
+	jsonBytes, err := json.Marshal(api.LoginCredentials{
+		Username: "testuser",
 		Password: plaintextPassword,
-	}
-	jsonBytes, err := json.Marshal(loginCredentials)
+	})
 	require.NoError(t, err)
 
-	req, err := http.NewRequest("POST", "/createuser", bytes.NewReader(jsonBytes))
+	req, err := http.NewRequest("POST", "/login", bytes.NewReader(jsonBytes))
 	assert.NoError(t, err)
 	req.Header.Set("Content-Type", "application/json")
-	InitCreateUserEndpoint(NewMockAuthService).RegisterEndpoints(router)
+	InitLoginEndpoint(NewMockAuthService).RegisterEndpoints(router)
 
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	var got database.PublicUser
+	var got api.StatusLoginSuccess
 	err = json.Unmarshal(w.Body.Bytes(), &got)
 	assert.NoError(t, err)
-	assert.Equal(t, *expectedUser, got)
+	assert.Equal(t, "json_token", got.Token)
 }
 
-func TestCreateUserFailure(t *testing.T) {
+func TestLoginFailure(t *testing.T) {
 	plaintextPassword := "securepassword"
 
 	NewMockAuthService := mockUserService.NewMockAuthService(t)
 	NewMockAuthService.EXPECT().
-		AddUser("newuser", plaintextPassword).
-		Return(nil, fmt.Errorf("an error")).Once()
+		AuthenticateUser("testuser", plaintextPassword).
+		Return("", fmt.Errorf("authentication failed")).Once()
 
 	router := gin.Default()
 	loginCredentials := api.LoginCredentials{
-		Username: "newuser",
+		Username: "testuser",
 		Password: plaintextPassword,
 	}
 	jsonBytes, err := json.Marshal(loginCredentials)
 	require.NoError(t, err)
 
-	req, err := http.NewRequest("POST", "/createuser", bytes.NewReader(jsonBytes))
+	req, err := http.NewRequest("POST", "/login", bytes.NewReader(jsonBytes))
 	assert.NoError(t, err)
 	req.Header.Set("Content-Type", "application/json")
-	InitCreateUserEndpoint(NewMockAuthService).RegisterEndpoints(router)
+	InitLoginEndpoint(NewMockAuthService).RegisterEndpoints(router)
 
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-	var got api.StatusBadRequestResponse
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+	var got api.StatusLoginFail
 	err = json.Unmarshal(w.Body.Bytes(), &got)
 	assert.NoError(t, err)
-	expected := api.StatusBadRequestResponse{Error: "failed to create user: an error"}
+	expected := api.StatusLoginFail{Error: "authentication failed: authentication failed"}
 	assert.Equal(t, expected, got)
 }
