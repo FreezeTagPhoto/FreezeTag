2962c7fca454b65a089446c07420b56894338c23
Ethan <u0911543@utah.edu>
2026-01-29 19:10:02 -0700

    added GET request to check if authenticated

---
+++ backend/middleware/check_auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"freezetag/backend/pkg/services"
+	"github.com/gin-gonic/gin"
+)
+
+func CheckAuth(auth services.AuthService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		JWT, err := c.Cookie("token")
+		if err != nil || JWT == "" {
+			c.Set("authenticated", false)
+			c.Next()
+			return
+		}
+
+		_, err = auth.ValidateJWT(JWT)
+		if err != nil {
+			c.Set("authenticated", false)
+			c.Next()
+			return
+		}
+		c.Set("authenticated", true)
+		c.Next()
+	}
+}
