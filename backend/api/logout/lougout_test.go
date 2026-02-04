9f2680d2050adb9a83d8a78b1ef78dabbd00d784
Ethan <u0911543@utah.edu>
2026-02-04 13:09:39 -0700

    added testing to the logout endpoint

---
+++ backend/api/logout/lougout_test.go
@@ -0,0 +1,37 @@
+package logout
+
+import (
+	"encoding/json"
+	"freezetag/backend/api"
+	mockUserService "freezetag/backend/mocks/AuthService"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogout(t *testing.T) {
+
+	router := gin.Default()
+	w := httptest.NewRecorder()
+
+	NewMockAuthService := mockUserService.NewMockAuthService(t)
+
+	req, err := http.NewRequest("POST", "/logout", nil)
+	require.NoError(t, err)
+	req.AddCookie(&http.Cookie{Name: "token", Value: "TOKENSTRING"})
+	InitLogoutEndpoint(NewMockAuthService).RegisterEndpoints(router)
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, -1, w.Result().Cookies()[0].MaxAge)
+	assert.Equal(t, "", w.Result().Cookies()[0].Value)
+	var got api.StatusLogoutSuccess
+	err = json.Unmarshal(w.Body.Bytes(), &got)
+	assert.NoError(t, err)
+	expected := api.StatusLogoutSuccess{Status: "ok"}
+	assert.Equal(t, expected, got)
+}
