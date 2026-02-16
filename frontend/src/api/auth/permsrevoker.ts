a65b0e8d65ad87de6f0405aab1791c1de7ef3889
bjonsson <brayden.f.jonsson@utah.edu>
2026-02-16 11:41:55 -0700

    Add perms adder and perms revoker api handlers

---
+++ frontend/src/api/auth/permsrevoker.ts
@@ -0,0 +1,54 @@
+import SERVER_ADDRESS from "@/api/common/serveraddress";
+import { ApiHandler, Method, RequestError } from "@/api/common/apihandler";
+import { Result, Err } from "@/common/result";
+
+export type PermsRevokeResult = Result<
+    { message: string },
+    { status: number; message: string }
+>;
+type PermsRevokeResponse = { message: string };
+
+export default async function PermsRevoke(
+    user_id: number,
+    permissions: string[],
+): Promise<PermsRevokeResult> {
+    return add_perms_with_handler(
+        ApiHandler<PermsRevokeResponse>(
+            SERVER_ADDRESS + "user/permissions/",
+            false,
+        )(Method.DELETE),
+        user_id,
+        permissions,
+    );
+}
+
+async function add_perms_with_handler(
+    handler: (
+        data: BodyInit,
+    ) => Promise<Result<PermsRevokeResponse, RequestError>>,
+    user_id: number,
+    permissions: string[],
+): Promise<PermsRevokeResult> {
+    const query = `${user_id}?permissions=${permissions.join()}`;
+    const request_result = await handler(query);
+
+    if (!request_result.ok) {
+        const status = request_result.error.status_code;
+        if (status == 400)
+            return Err({
+                status,
+                message: (
+                    (await request_result.error.response.json()) as {
+                        error: string;
+                    }
+                ).error,
+            });
+        else
+            return Err({
+                status,
+                message: await request_result.error.response.text(),
+            });
+    }
+
+    return request_result;
+}
