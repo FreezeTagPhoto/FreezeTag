6999fa9fc7ed05cc673f722bb3711b94d7bd14c6
Sathya Tadinada <sathya.tadinada@utah.edu>
2026-02-18 21:07:26 -0700

    Finished out skeleton pages for plugins and tags

--- frontend/src/app/(app)/plugins/page.tsx
+++ frontend/src/app/(app)/plugins/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <h1 className={styles.h1}>Tag Management</h1>
+            <h1 className={styles.h1}>Plugin Management</h1>
             <p className={styles.create_user}>Under Construction</p>
         </main>
     );
