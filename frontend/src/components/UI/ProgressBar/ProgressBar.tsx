e44341e757db7cdb1350fbdd19af9a4fa8db4346
bjonsson <brayden.f.jonsson@utah.edu>
2026-02-24 21:22:03 -0700

    Make progress bar even more extensible

--- frontend/src/components/UI/ProgressBar/ProgressBar.tsx
+++ frontend/src/components/UI/ProgressBar/ProgressBar.tsx
@@ -2,9 +2,11 @@ import styles from "./ProgressBar.module.css";
 export type ProgressBarProps = {
     progress: number;
     className?: string;
+    color?: "red" | "green";
 };
 
 export default function ProgressBar(props: ProgressBarProps) {
+    const colorClassName = `${props.color === "green" ? styles.progressBarGreen : props.color === "red" ? styles.progressBarRed : ""}`;
     return (
         <div>
             <label htmlFor="job-progress" className={styles.hidden}>
@@ -13,7 +15,7 @@ export default function ProgressBar(props: ProgressBarProps) {
             <progress
                 id="job-progress"
                 value={props.progress}
-                className={`${styles.progressBar} ${props.className ?? ""}`}
+                className={`${styles.progressBar} ${colorClassName} ${props.className ?? ""}`}
             >
                 {props.progress * 100}%
             </progress>
