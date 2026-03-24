15cbd5f632bb5fec3822cad0d1b569118325314d
bjonsson <brayden.f.jonsson@utah.edu>
2026-03-23 20:11:06 -0600

    Fix datatype for hooks/plugins

--- frontend/src/api/plugins/pluginshelpers.ts
+++ frontend/src/api/plugins/pluginshelpers.ts
@@ -1,10 +1,11 @@
-export type Hook = { signature: string; type: string };
+export type Hook = { friendly_name: string; signature: string; type: string };
 
 export type Hooks = Record<string, Hook>;
 
 export type Plugin = {
     enabled: boolean;
     name: string;
+    friendly_name: string;
     version: string;
     hooks: Hooks;
 };
