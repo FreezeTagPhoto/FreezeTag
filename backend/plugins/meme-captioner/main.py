da47a1f64cfe61783977b30d5fb0a29db7498f8b
bjonsson <brayden.f.jonsson@utah.edu>
2026-03-28 13:26:17 -0600

    Improve formatting for meme captioner

--- backend/plugins/meme-captioner/main.py
+++ backend/plugins/meme-captioner/main.py
@@ -4,9 +4,13 @@ from freezetag.message import get_image
 
 from PIL import ImageDraw, ImageFont
 
+
 @single_image
 def make_caption_form(_img, id):
-    return SendFormAction(f"""<form><input type="hidden" value="{id}" name="id"></input><label for="top_text">Top Text:</label><input type="text" name="top_text" id="top_text" required></input><label for="bottom_text">Bottom Text:</label><input type="text" name="bottom_text" id="bottom_text" required></input></form>""")
+    return SendFormAction(
+        f"""<form><input type="hidden" value="{id}" name="id"></input><label for="top_text">Top Text:</label><input type="text" name="top_text" id="top_text" required></input><label for="bottom_text">Bottom Text:</label><input type="text" name="bottom_text" id="bottom_text" required></input></form>"""
+    )
+
 
 @form_data
 def process_caption_form(data):
@@ -20,12 +24,13 @@ def process_caption_form(data):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("impact.ttf", font_size)
 
-    draw.text((0, 0),top,(255,255,255),font=font)
+    draw.text((0, 0), top, (255, 255, 255), font=font)
 
     width, height = image.size
-    draw.text((0, height - font_size),bottom,(255,255,255),font=font)
+    draw.text((0, height - font_size), bottom, (255, 255, 255), font=font)
 
     return AddImageAction(f"{id}_meme", "png", image)
+
 
 if __name__ == "__main__":
     freezetag.run()
