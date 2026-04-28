08fe831fddaa0dde93db9290b10b95b50e0dc63e
Max Petersen <maxvonpetersen@gmail.com>
2026-04-27 21:16:30 -0600

    tweak backend coverage code to report correct percentage and fail on coverage too low

--- backend/test.sh
+++ backend/test.sh
@@ -9,4 +9,10 @@ mkdir -p coverage
 go test -coverprofile="coverage/coverage.out" -covermode count -tags=test,$TAGS $(go list ./... | grep -v -e /mocks/ -e /cmd/)
 go tool cover -html=coverage/coverage.out -o coverage/coverage.html
 GOFLAGS="-tags=test,$TAGS" go tool gocover-cobertura < coverage/coverage.out > coverage/cobertura-coverage.xml
-go tool cover -func coverage/coverage.out | tail -n 1
+coverage=$(go tool cover -func coverage/coverage.out | tail -n 1 | awk '{print $3}')
+echo "overall coverage: $coverage"
+required=80.0%
+if [ "$(echo "${required%\%} > ${coverage%\%}" | bc)" -eq 1 ]; then
+    echo "coverage does not meet requirements"
+    exit 1
+fi
